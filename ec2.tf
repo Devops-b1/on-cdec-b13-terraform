a7ad3f64cd25856ae0e359083985743f33957841
skalsait <skalsait@dyl.com>
2024-11-25 20:55:58 +0530

    --- Auto Git Commit ---

--- ec2.tf
+++ ec2.tf
@@ -22,7 +22,10 @@ resource "aws_security_group" "my_sg" {
         cidr_blocks      = ["0.0.0.0/0"]
     }
     egress {
-        
+        from_port        = 80
+        to_port          = 80
+        protocol         = "TCP"
+        cidr_blocks      = ["0.0.0.0/0"]
     }
 }
 

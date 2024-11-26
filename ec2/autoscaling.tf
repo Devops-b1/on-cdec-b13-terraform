3ec50efdf603a60471f5710949334940f0225f13
skalsait <skalsait@dyl.com>
2024-11-26 20:20:12 +0530

    --- Auto Git Commit ---

--- ec2/autoscaling.tf
+++ ec2/autoscaling.tf
@@ -1,7 +1,7 @@
 resource "aws_launch_configuration" "lc_home" {
-    image_id = 
-    instance_type
-    key_name
-    security_groups
+    image_id = var.image_id
+    instance_type = var.instance_type
+    key_name = var.key_pair
+    security_groups = 
     user_data
 }

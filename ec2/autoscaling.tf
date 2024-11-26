00fb96a151cd0f85aae474c0ad8e6126294141eb
skalsait <skalsait@dyl.com>
2024-11-26 20:21:43 +0530

    --- Auto Git Commit ---

--- ec2/autoscaling.tf
+++ ec2/autoscaling.tf
@@ -8,5 +8,6 @@ resource "aws_launch_configuration" "lc_home" {
     yum install httpd -y
     systemctl start httpd
     systmectl enable httpd
-    echo "<h1> Hello World </h1> <br> <h2> Welcome to Cloudblitz </h2>
+    echo "<h1> Hello World </h1> <br> <h2> Welcome to Cloudblitz </h2>" > /var/www/html/index.html
+    EOF
 }

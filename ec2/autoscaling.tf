ebc6775c49ad89d91f3a960ed5e1bd78f7cd5bfa
skalsait <skalsait@dyl.com>
2024-11-26 20:21:23 +0530

    --- Auto Git Commit ---

--- ec2/autoscaling.tf
+++ ec2/autoscaling.tf
@@ -5,5 +5,8 @@ resource "aws_launch_configuration" "lc_home" {
     security_groups = 
     user_data = <<EOF
     #!/bin/bash
-    apt update -y
+    yum install httpd -y
+    systemctl start httpd
+    systmectl enable httpd
+    echo "<h1> Hello Worl </h1> <br> <h2> Welcome to Cloudblitz </h2>
 }

b49e837471275bf7abe3b8403a72827d4247b74d
skalsait <skalsait@dyl.com>
2024-11-26 21:07:39 +0530

    --- Auto Git Commit ---

--- ec2/loadbalancer.tf
+++ ec2/loadbalancer.tf
@@ -48,6 +48,6 @@ resource "aws_lb_listener" "my_lb_listener" {
   protocol          = "HTTP"
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.
+    target_group_arn = aws_lb_target_group.tg_home.arn
   }
 }

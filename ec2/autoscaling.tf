b226e898bd94e01718b57fdd0adcea73d6d74b5a
skalsait <skalsait@dyl.com>
2024-11-26 20:44:16 +0530

    --- Auto Git Commit ---

--- ec2/autoscaling.tf
+++ ec2/autoscaling.tf
@@ -75,7 +75,7 @@ resource "aws_autoscaling_group" "asg_home" {
 
 resource "aws_autoscaling_policy" "asg_policy" {
   autoscaling_group_name = aws_autoscaling_group.asg_home.name
-  name                   = "foo"
+  name                   = "asg_policy_home"
   policy_type            = "PredictiveScaling"
   predictive_scaling_configuration {
     metric_specification {

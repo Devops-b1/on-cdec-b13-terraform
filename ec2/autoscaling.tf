39812100aa14f305369a3b9c8800e8c59df039f8
skalsait <skalsait@dyl.com>
2024-11-26 20:50:26 +0530

    --- Auto Git Commit ---

--- ec2/autoscaling.tf
+++ ec2/autoscaling.tf
@@ -110,7 +110,7 @@ resource "aws_autoscaling_policy" "asg_policy_laptop" {
   }
 }
 
-resource "aws_autoscaling_group" "asg_home" {
+resource "aws_autoscaling_group" "asg_mobile" {
   name                 = "asg-home"
   max_size             = 5
   min_size             = 2

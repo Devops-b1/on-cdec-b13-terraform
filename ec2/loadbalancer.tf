2f9cdb1a25245917e9442986f66f02672f2411c7
skalsait <skalsait@dyl.com>
2024-11-26 20:55:57 +0530

    --- Auto Git Commit ---

--- ec2/loadbalancer.tf
+++ ec2/loadbalancer.tf
@@ -1,5 +1,5 @@
-resource "aws_lb_target_group" "test" {
-  name     = "tf-example-lb-tg"
+resource "aws_lb_target_group" "tg_home" {
+  name     = "tg-home"
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id

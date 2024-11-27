7bff0125425c218079c45ec326cfb8c2ceb07e5f
skalsait <skalsait@dyl.com>
2024-11-27 21:31:20 +0530

    --- Auto Git Commit ---

--- cloudblitz/main.tf
+++ cloudblitz/main.tf
@@ -24,7 +24,7 @@ module "ec2_module" {
 resource "aws_security_group" "my_sg" {
     name = "my-sg"
     description = "allow HTTP Port"
-    vpc_id = module.
+    vpc_id = module.vpc_module.vpc_id
     ingress {
         from_port        = 80
         to_port          = 80

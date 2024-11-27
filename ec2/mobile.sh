fe7cb353b7b3c5d69cf65924ad61079109ddab82
skalsait <skalsait@dyl.com>
2024-11-27 20:27:41 +0530

    --- Auto Git Commit ---

--- ec2/mobile.sh
+++ ec2/mobile.sh
@@ -1,8 +1,6 @@
-<<EOF
-    #!/bin/bash
-    yum install httpd -y
-    systemctl start httpd
-    systmectl enable httpd
-    mkdir /var/www/html/mobile
-    echo "<h1> SALE SALE SALE on mobile Page" > /var/www/html/mobile/index.html
-    EOF
+#!/bin/bash
+yum install httpd -y
+systemctl start httpd
+systmectl enable httpd
+mkdir /var/www/html/mobile
+echo "<h1> SALE SALE SALE on mobile Page" > /var/www/html/mobile/index.html

ddbc60a78241dd295a6aa603d5fcc6b6df56a14d
skalsait <skalsait@dyl.com>
2024-11-27 20:56:25 +0530

    --- Auto Git Commit ---

--- cloudblitz/modules/vpc/main.tf
+++ cloudblitz/modules/vpc/main.tf
@@ -34,3 +34,14 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
+resource "aws_default_route_table" "r" {
+  default_route_table_id = "${aws_vpc.foo.default_route_table_id}"
+
+  route {
+    # ...
+  }
+
+  tags = {
+    Name = "default table"
+  }
+}

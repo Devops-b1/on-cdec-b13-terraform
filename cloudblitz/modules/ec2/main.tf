79174b5624e481b3f2c1062adbec7d98e1839843
skalsait <skalsait@dyl.com>
2024-11-27 21:02:46 +0530

    --- Auto Git Commit ---

--- cloudblitz/modules/ec2/main.tf
+++ cloudblitz/modules/ec2/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "private_instance" {
     instance_type = var.instance_type
     vpc_security_group_ids = var.security_group_id
     key_name = var.key_pair
-
+    
 }
 
 resource "aws_instance" "public_instance" {

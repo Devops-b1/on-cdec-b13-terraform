17ef830e561a4da24d663a2d113da0569f64d6ac
skalsait <skalsait@dyl.com>
2024-11-27 20:50:14 +0530

    --- Auto Git Commit ---

--- cloudblitz/modules/vpc/main.tf
+++ cloudblitz/modules/vpc/main.tf
@@ -15,9 +15,9 @@ resource "aws_subnet" "private_subnet" {
   }
 }
 
-resource "aws_subnet" "private_subnet" {
+resource "aws_subnet" "public_subnet" {
   vpc_id     = aws_vpc.cbz_vpc.id
-  cidr_block = var.private_subnet_cidr
+  cidr_block = var.public_subnet_cidr
 
   tags = {
     Name = "${var.project}-private-subnet"

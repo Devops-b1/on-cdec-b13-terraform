e132c75b1896ed28316c2a0faf7dc8911d8f2d00
skalsait <skalsait@dyl.com>
2024-11-27 20:22:00 +0530

    --- Auto Git Commit ---

--- ec2/autoscaling.tf
+++ ec2/autoscaling.tf
@@ -70,8 +70,8 @@ resource "aws_autoscaling_group" "asg_home" {
   min_size             = 2
   desired_capacity     = 2
   launch_template {
-    id = aws_launch_template.
-    version = 
+    id = aws_launch_template.lt_home.id
+    version = aws_launch_template.lt_home.latest_version
   }
   vpc_zone_identifier  = [var.subnet_id_1, var.subnet_id_2]
   target_group_arns = [aws_lb_target_group.tg_home.arn]

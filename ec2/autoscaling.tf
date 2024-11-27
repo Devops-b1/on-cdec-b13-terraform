090acf0cf5b86d262d5c7bd11d01b566f894b78e
skalsait <skalsait@dyl.com>
2024-11-27 20:22:10 +0530

    --- Auto Git Commit ---

--- ec2/autoscaling.tf
+++ ec2/autoscaling.tf
@@ -96,8 +96,10 @@ resource "aws_autoscaling_group" "asg_laptop" {
   max_size             = 5
   min_size             = 2
   desired_capacity     = 2
-  launch_configuration = aws_launch_configuration.lc_laptop.name
-  vpc_zone_identifier  = [var.subnet_id_1, var.subnet_id_2]
+  launch_template {
+    id = aws_launch_template.lt_home.id
+    version = aws_launch_template.lt_home.latest_version
+  }  vpc_zone_identifier  = [var.subnet_id_1, var.subnet_id_2]
   target_group_arns = [aws_lb_target_group.tg_laptop.arn]
 }
 

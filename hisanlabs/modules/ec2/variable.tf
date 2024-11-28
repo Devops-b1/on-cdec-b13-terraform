64b175f1576021a4a8d17ad63c9523c7d06b8ba5
skalsait <skalsait@dyl.com>
2024-11-28 20:09:18 +0530

    --- Auto Git Commit ---

---
+++ hisanlabs/modules/ec2/variable.tf
@@ -0,0 +1,7 @@
+variable "image_id" {}
+variable "instance_type" {}
+variable "security_group_id" {}
+variable "key_pair" {}
+variable "private_subnet_id" {}
+variable "public_subnet_id" {}
+
---
+++ hisanlabs/modules/vpc/variable.tf
@@ -0,0 +1,6 @@
+variable "cidr" {}
+variable "project" {}
+variable "env" {}
+variable "private_subnet_cidr" {}
+variable "public_subnet_cidr" {}
+

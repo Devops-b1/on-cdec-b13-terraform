7ae5dcabed16caaa8da348cf9ae116c4965eaadc
skalsait <skalsait@dyl.com>
2024-12-12 20:27:26 +0530

    --- Auto Git Commit ---

--- workspace/ec2.tf
+++ workspace/ec2.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "my_instance" {
-    ami = ""
-    instance_type = ""
+    ami = "ami-012967cc5a8c9f891"
+    instance_type = "t2.micro"
     key_name = ""
 }

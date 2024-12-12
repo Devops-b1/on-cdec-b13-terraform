d7fbdd5595095df36b31dc0a7a33e6c890eb71f2
skalsait <skalsait@dyl.com>
2024-12-12 20:43:18 +0530

    --- Auto Git Commit ---

--- loop/main.tf
+++ loop/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "my_instance" {
-    count = 5
+    count = 3
     ami = "ami-012967cc5a8c9f891"
     instance_type = "t2.micro"
     key_name = "new-key-nv"

46f5bb64dcc1f74e6e789cc0d33a30970317854c
skalsait <skalsait@dyl.com>
2024-12-12 20:58:20 +0530

    --- Auto Git Commit ---

--- loop/main.tf
+++ loop/main.tf
@@ -30,20 +30,3 @@ default = {
     medium  = "t2.micro"
 }
 }
-
-variable "instance_conf" {
-    default = {
-        instance1 = {
-            ami = "ami-012967cc5a8c9f891"
-            instance_type = "t2.micro"
-        }
-        instance2 = {
-            ami = "ami-012967ccskahfgjc9f891"
-            instance_type = "t3.micro"
-        }
-        instance3 = {
-            ami = "ami-ffdljbfs"
-            instance_type = "t4.micro"
-        }
-    }
-}

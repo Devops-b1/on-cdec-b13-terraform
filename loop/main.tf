468d29e9522b52348e73e86e24adead4b3e45e6e
skalsait <skalsait@dyl.com>
2024-12-12 20:42:48 +0530

    --- Auto Git Commit ---

---
+++ loop/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "my_instance" {
+    ami = "ami-012967cc5a8c9f891"
+    instance_type = "t2.micro"
+    key_name = "new-key-nv"
+    tags = {
+        Env = terraform.workspace
+    }
+}

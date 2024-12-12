13e8175590d623d7be84c3504371f8ae9dcb867e
skalsait <skalsait@dyl.com>
2024-12-12 20:56:40 +0530

    --- Auto Git Commit ---

--- loop/main.tf
+++ loop/main.tf
@@ -37,5 +37,13 @@ variable "instance_conf" {
             ami = "ami-012967cc5a8c9f891"
             instance_type = "t2.micro"
         }
+        instance2 = {
+            ami = "ami-012967ccskahfgjc9f891"
+            instance_type = "t3.micro"
+        }
+        instance3 = {
+            ami = "ami-012967ccskahfgjc9f891"
+            instance_type = "t3.micro"
+        }
     }
 }

a7f2db551bcf47c334fc93617d3e79758720c93a
skalsait <skalsait@dyl.com>
2024-12-12 20:35:57 +0530

    --- Auto Git Commit ---

--- workspace/ec2.tf
+++ workspace/ec2.tf
@@ -7,6 +7,6 @@ resource "aws_instance" "my_instance" {
     instance_type = "t2.micro"
     key_name = "new-key-nv"
     tags = {
-        Env = "terraform.workspace
-    }"
+        Env = "prod"
+    }
 }

90a8bb619520a09d04c05bcbacc0a2ea53e54390
skalsait <skalsait@dyl.com>
2024-12-13 21:08:17 +0530

    --- Auto Git Commit ---

--- import/main.tf
+++ import/main.tf
@@ -2,20 +2,9 @@ provider "aws" {
     region = "us-east-1"
 }
 
-resource "aws_instance" "demo_import_instance" {
-    instance_type = ""
-    ami = ""
-}
-
-resource "aws_instance" "demo_import_instance" {
-    instance_type = ""
-    ami = ""
-}
 # resource "aws_instance" "demo_import_instance" {
 #     instance_type = ""
 #     ami = ""
 # }
-resource "aws_instance" "demo_import_instance" {
-    instance_type = ""
-    ami = ""
-}
+
+

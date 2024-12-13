8d58119fa7ebf3043b64c2d513d6ee115efddda6
skalsait <skalsait@dyl.com>
2024-12-13 21:18:58 +0530

    --- Auto Git Commit ---

--- import/main.tf
+++ import/main.tf
@@ -17,7 +17,7 @@ resource "aws_instance" "my_instance" {
         source  = "./script.sh"
         destination = "/opt/script.sh"
 
-                connection {
+        connection {
             type = "ssh"
             user = "ec2-user"
             private_key = file("~/id_rsa")

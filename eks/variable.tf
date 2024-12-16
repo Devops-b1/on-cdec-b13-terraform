ffeb7542d0c5b4f77e49fbe93f2e6d9082bf7c3c
skalsait <skalsait@dyl.com>
2024-12-16 20:52:15 +0530

    --- Auto Git Commit ---

--- eks/variable.tf
+++ eks/variable.tf
@@ -1,7 +1,9 @@
 variable project {
-    
+    default = "cbz"
 }
-variable desired_nodes {}
+variable desired_nodes {
+    default = 2
+}
 variable max_nodes {}
 variable min_nodes {}
 variable node_instance_type {}
